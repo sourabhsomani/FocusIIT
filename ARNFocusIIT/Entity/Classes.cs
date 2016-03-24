f78941bb7e949180fb3e7506b00f5e77972022e8
sourabh somani <sourabh_somani2010@hotmail.com>
2016-03-24 18:57:08 +0530

    Classes & Courses DB

---
+++ ARNFocusIIT/Entity/Classes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ARNFocusIIT.Entity
+{
+    public class Classes
+    {
+        [Key]
+        public string ID { get; set; }
+        [Required(ErrorMessage="Class Name Can't Be Empty")]
+        public string ClassName { get; set; }
+        public string Added { get; set; }
+    }
+}
---
+++ ARNFocusIIT/Entity/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ARNFocusIIT.Entity
+{
+    public class Course
+    {
+        [Key]
+        public string ID { get; set; }
+        [Required(ErrorMessage="Course Name cannot be Empty")]
+        public string CourseName { get; set; }
+        public DateTime Added { get; set; }
+    }
+}
